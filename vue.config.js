206aea75b5fff96e0fbe468adce92b317379e6ee
Jason <atwill007@163.com>
2019-05-22 18:19:14 +0800

    feat: 过滤views文件夹下_开头的目录

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,11 @@ const path = require('path')
 
 let pages = {}
 
-const files = fs.readdirSync(path.resolve(__dirname, './src/views'))
+const files = fs.readdirSync(resolve('./src/views')).filter(item => !/^_.*/.test(item))
 files.forEach(file => {
   pages[file] = {
     entry: `src/views/${file}/`,
+    title: file,
     template: 'public/index.html',
     filename: `${file}.html`,
     chunks: ['chunk-vendors', 'chunk-common', file]
@@ -17,11 +18,19 @@ module.exports = {
   pages,
   lintOnSave: true,
   devServer: {
+    port: 9025,
+    compress: true, // 开启压缩
     proxy: {
       '/search': {
         target: 'https://api.github.com',
         ws: true,
         changeOrigin: true
+      },
+      '/api/v2': {
+        target: 'http://app.jason.alpha.dev1.kaolalicai.cn'
+      },
+      '/activity/api': {
+        target: 'http://app.jason.alpha.dev1.kaolalicai.cn'
       }
     }
   }
